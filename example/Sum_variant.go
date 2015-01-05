0857bf1eed4342f540dd7f5afc0f263550258d7c
Karol Marcjan <karol.marcjan@gmail.com>
2015-01-05 09:58:13 +0100

    Fmt genrated code in example

--- example/Sum_variant.go
+++ example/Sum_variant.go
@@ -1,16 +1,12 @@
-
 package example
 
-
-
-// A Sum is one of 
-// 
+// A Sum is one of
+//
 //     - Circle
 //     - Rectangle
 type Sum interface {
-	 Circle() (Circle, bool)
-	 Rectangle() (Rectangle, bool)
-	
+	Circle() (Circle, bool)
+	Rectangle() (Rectangle, bool)
 }
 
 // A SumExhaustive is a Sum that can be use to check
@@ -18,54 +14,42 @@ type Sum interface {
 type SumExhaustive struct {
 	Sum
 
-	 CircleCalled bool
-	 RectangleCalled bool
-	
+	CircleCalled    bool
+	RectangleCalled bool
 }
 
 // Checks whether all the variants of the Sum were considered.
 func (se SumExhaustive) Exhaustive() bool {
-	
+
 	if !se.CircleCalled {
 		return false
 	}
-	
+
 	if !se.RectangleCalled {
 		return false
 	}
-	
 
 	return true
 }
 
+// Circle implements the corresponding method of Sum on the Circle type.
+func (sv Circle) Circle() (Circle, bool) {
+	return sv, true
+}
 
-	
-		// Circle implements the corresponding method of Sum on the Circle type.
-		 func (sv Circle) Circle() (Circle, bool) {
-				return sv, true
-			}
-		
-	
-		// Rectangle implements the corresponding method of Sum on the Circle type.
-		 func (_ Circle) Rectangle() (Rectangle, bool) {
-				var v Rectangle
-				return v, false
-			}
-		
-	
+// Rectangle implements the corresponding method of Sum on the Circle type.
+func (_ Circle) Rectangle() (Rectangle, bool) {
+	var v Rectangle
+	return v, false
+}
 
-	
-		// Circle implements the corresponding method of Sum on the Rectangle type.
-		 func (_ Rectangle) Circle() (Circle, bool) {
-				var v Circle
-				return v, false
-			}
-		
-	
-		// Rectangle implements the corresponding method of Sum on the Rectangle type.
-		 func (sv Rectangle) Rectangle() (Rectangle, bool) {
-				return sv, true
-			}
-		
-	
+// Circle implements the corresponding method of Sum on the Rectangle type.
+func (_ Rectangle) Circle() (Circle, bool) {
+	var v Circle
+	return v, false
+}
 
+// Rectangle implements the corresponding method of Sum on the Rectangle type.
+func (sv Rectangle) Rectangle() (Rectangle, bool) {
+	return sv, true
+}
