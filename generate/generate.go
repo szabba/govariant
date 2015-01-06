9d1f1798b1859c13f1e9099c6a3d48aa00280386
Karol Marcjan <karol.marcjan@gmail.com>
2015-01-06 23:56:36 +0100

    Pass TestGeneratedSourceDeclaresTypeWithNameGiven

--- generate/generate.go
+++ generate/generate.go
@@ -1,7 +1,15 @@
 package generate
 
+import "fmt"
+
+var pkgFormat = `
+package pkg
+
+type %s interface{}
+`
+
 // Generate returns the source for a file that defines a sum type when given
 // it's name and the names of it's variants.
 func Generate(sumType string, variants ...string) string {
-	return "package pkg"
+	return fmt.Sprintf(pkgFormat, sumType)
 }
--- generate/generate_test.go
+++ generate/generate_test.go
@@ -1,6 +1,7 @@
 package generate
 
 import (
+	"go/ast"
 	"go/parser"
 	"go/token"
 	"testing"
@@ -17,4 +18,45 @@ func TestGeneratedSourceParsesWithoutErrors(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+}
+
+func TestGeneratedSourceDeclaresTypeWithNameGiven(t *testing.T) {
+	sumType := "Sum"
+	variants := []string{"X", "Y", "Z"}
+
+	src := Generate(sumType, variants...)
+
+	fset := token.NewFileSet()
+	f, _ := parser.ParseFile(fset, "src.go", src, 0)
+
+	found := false
+	for _, typ := range typesInFile(f) {
+		if typ.Name.Name == sumType {
+			found = true
+		}
+	}
+
+	if !found {
+		t.Errorf("type %s not declared in generated sources", sumType)
+	}
+}
+
+func typesInFile(f *ast.File) []*ast.TypeSpec {
+	var types []*ast.TypeSpec
+
+	for _, decl := range f.Decls {
+		decl := decl.(*ast.GenDecl)
+
+		if decl.Tok == token.TYPE {
+
+			for _, spec := range decl.Specs {
+				if typ, ok := spec.(*ast.TypeSpec); ok {
+
+					types = append(types, typ)
+				}
+			}
+		}
+	}
+
+	return types
 }
